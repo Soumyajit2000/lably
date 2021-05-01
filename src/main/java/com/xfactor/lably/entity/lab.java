6eba7bd895e74cb5f3350c197fb6184782bbc11d
Soumyajit2000 <soumyajitmondal994@gmail.com>
2021-05-01 14:44:10 +0530

    Commit 2: add

--- src/main/java/com/xfactor/lably/entity/lab.java
+++ src/main/java/com/xfactor/lably/entity/lab.java
@@ -1,34 +1,45 @@
 package com.xfactor.lably.entity;
 
-public class lab{
+public class Lab {
+
     private String name;
-    private String address;
+
     private String phone;
+
+    private String address;
+
     private String pincode;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getPincode() {
         return pincode;
     }
+
     public void setPincode(String pincode) {
         this.pincode = pincode;
     }
 
-    
 }
